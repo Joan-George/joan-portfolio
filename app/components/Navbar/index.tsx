2029759b795ef0fb31c773a99edbdbf5e3b80341
Joan_George_01 <jgtharchius@raisetech.io>
2024-01-15 11:14:50 +0530

    navigation added

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+
+const navItems = [
+	{ name: "Projects", path: "/projects" },
+	{ name: "Experience", path: "/experience" },
+];
+
 const Navbar = () => {
-	return <>Navbar</>;
+	return (
+		<>
+			<ul className="flex-col gap-2">
+				{navItems.map((nav) => (
+					<li key={nav.path}>
+						<Link href={nav.path}>{nav.name}</Link>
+					</li>
+				))}
+			</ul>
+		</>
+	);
 };
 
 export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Navbar from "./components/Navbar";
-import Project from "./components/Project";
 
 export default function Home() {
 	return (
 		<main className="">
 			<Navbar></Navbar>
-			<Project></Project>
 		</main>
 	);
 }
